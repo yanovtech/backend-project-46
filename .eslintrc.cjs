85d400f94ff86205a7ac8828251895c9c9d88cf4
yanovtech <yanovlive@gmail.com>
2025-04-16 11:58:44 +0300

    add commander.js and description for gendiff.js

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+};
