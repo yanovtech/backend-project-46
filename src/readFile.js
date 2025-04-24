f671dc3c7aceb32221f0e8316fa188cc432a6cf0
yanovtech <yanovlive@gmail.com>
2025-04-24 15:40:41 +0300

    fix bugs again 8

--- src/readFile.js
+++ src/readFile.js
@@ -1,11 +1,11 @@
 import { readFileSync } from 'node:fs';
-import { cwd } from 'node:process';
 import path from 'node:path';
+import { cwd } from 'node:process';
 
 export default (filePath) => {
   try {
-    const pwd = cwd();
-    return readFileSync(path.resolve(pwd, filePath), 'utf-8');
+    const fullPath = path.isAbsolute(filePath) ? filePath : path.resolve(cwd(), filePath);
+    return readFileSync(fullPath, 'utf-8');
   } catch (e) {
     throw new Error(`Не удалось прочитать файл: ${filePath}\n${e.message}`);
   }
