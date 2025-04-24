a8f19f097b09016f5ebf41c0e05df882a04854df
yanovtech <yanovlive@gmail.com>
2025-04-24 14:54:39 +0300

    fix bugs again 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,15 +2,23 @@
 import { Command } from 'commander';
 import makeDiff from '../src/makeDiff.js';
 
-const program = new Command();
+const buildProgram = () => {
+  const program = new Command();
 
-program
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(makeDiff(filepath1, filepath2, program.opts().format));
-  });
+  program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-f, --format [type]', 'output format', 'stylish')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+      const opts = program.opts();
+      console.log(makeDiff(filepath1, filepath2, opts.format));
+    });
 
-program.parse();
+  return program;
+};
+
+if (process.argv[1] === new URL(import.meta.url).pathname) {
+  buildProgram().parse(process.argv);
+}
