fe377434d1ebb6d94820256a4a7f6087bd34fdda
yanovtech <yanovlive@gmail.com>
2025-04-25 00:13:53 +0300

    fix bugs again 9

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,23 +2,17 @@
 import { Command } from 'commander';
 import gendiff from '../src/genDiff.js';
 
-const buildProgram = () => {
-  const program = new Command();
+const program = new Command();
 
-  program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-      const opts = program.opts();
-      console.log(gendiff(filepath1, filepath2, opts.format));
-    });
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    const result = gendiff(filepath1, filepath2, options.format);
+    console.log(result);
+  });
 
-  return program;
-};
-
-if (process.argv[1] === new URL(import.meta.url).pathname) {
-  buildProgram().parse(process.argv);
-}
+program.parse();
